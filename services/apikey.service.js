b7fa4871566b4dd526f65e19a7908b2f999c168c
Dhalintin <uchexdhalitin@gmail.com>
2024-05-31 23:50:50 +0100

    Completed the upload function

--- services/apikey.service.js
+++ services/apikey.service.js
@@ -1,6 +1,11 @@
 const Apikey = require('../models/apikey.model');
 
 class ApiKeyService{
+    async findApikey(apiKey){
+        const newApikey = await Apikey.findOne({ apiKey });
+        return newApikey;
+    }
+    
     async storeApiKey(apiKey, userId){
         const newApikey = new Apikey({ apiKey, userId });
         return await newApikey.save();
